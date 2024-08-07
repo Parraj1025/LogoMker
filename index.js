3b62d937c42bfbb0776dfec7c9c9a9ee7cba5c0c
Juan Parra <parrajuan1025@gmail.com>
2024-08-06 20:44:49 -0400

    revision of LogoMaker

---
+++ index.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer');
+const path = require('path')
+const fs = require('fs');
+
+async function createSVG(fileName, answers) {
+    
+    let svgData = ''
+    let baseScript = 
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <g>`
+    let txtScript = `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.txtColor}">${answers.text}</text></g></svg>`
+    if (answers.shape == 'square'){
+        const shapeInfo =`<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}" />`;
+        baseScript += shapeInfo;
+        baseScript += txtScript;
+        svgData = baseScript;
+        console.log(svgData);
+    }
+    if(answers.shape == 'circle') {
+        const shapeInfo = `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
+        baseScript += shapeInfo;
+        baseScript += txtScript;
+        svgData = baseScript;
+        console.log(svgData)
+    }
+    if(answers.shape == 'triangle'){
+        const shapeInfo = `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`
+        baseScript += shapeInfo;
+        baseScript += txtScript;
+        svgData = baseScript
+    }
+
+    const folderPath = path.join(__dirname, './lib/examples/')
+    const filePath = path.join(folderPath, `${answers.text}.svg`)
+
+    fs.writeFile(filePath, svgData, (err) => {
+    console.log(err)
+    })
+    return svgData
+}
+
+async function collectData() {
+   await inquirer.prompt([
+        {
+            name: 'shape',
+            type: 'list',
+            choices: ['square','circle','triangle'],
+            message: 'select shape'
+        },
+        {
+            name: 'shapeColor',
+            type: 'input',
+            message: 'select shape color'
+        },
+        {
+            name: 'text',
+            type: 'input',
+            message: 'enter logo text'
+        },
+        {
+            name: 'txtColor',
+            type: 'input',
+            message: 'enter text color'
+        }
+    ]).then((answers) => {
+        createSVG(answers.text, answers)
+        return createSVG(answers.text, answers)
+    })
+}
+
+module.exports = {collectData,createSVG}
